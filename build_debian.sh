ac575f89942918ba07b83eccd6ca2a9b1da2b221
gpunathilell <gpunathilell@nvidia.com>
2024-07-08 15:09:10 +0000

    Added debug messages for DNS

--- build_debian.sh
+++ build_debian.sh
@@ -412,6 +412,22 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
 	lsof                    \
 	sysstat
 
+filename="$FILESYSTEM_ROOT/sbin/resolvconf"
+sudo sed -i '/report_err \"Run lock held by another process for longer than $LOCK_WAIT_S seconds\"; exit 1; /a \    echo \"${MYNAME} Run lock obtained by PID $$, PPID $PPID\" >&2 ;' $FILESYSTEM_ROOT/sbin/resolvconf
+sudo sed -i '/: >| \"$ENABLE_UPDATES_FLAGFILE\" || exit 1/c\	: >| \"$ENABLE_UPDATES_FLAGFILE\" || { report_err \"Unable to create ENABLE_UPDATES_FLAGFILE\"; exit 1 ; }' $FILESYSTEM_ROOT/sbin/resolvconf
+sudo sed -i '/rm -f "$ENABLE_UPDATES_FLAGFILE" || exit 1/c\	rm -f \"$ENABLE_UPDATES_FLAGFILE\" || { report_err \"Unable to remove ENABLE_UPDATES_FLAGFILE\"; exit 1 ; }' $FILESYSTEM_ROOT/sbin/resolvconf
+sudo sed -i '/rm -f "$ENABLE_UPDATES_FLAGFILE" || exit 1/c\	rm -f \"$ENABLE_UPDATES_FLAGFILE\" || { report_err \"Unable to remove ENABLE_UPDATES_FLAGFILE\"; exit 1 ; }' $FILESYSTEM_ROOT/sbin/resolvconf
+repl_text="{report_err \"ENABLE_UPDATES_FLAGFILE does not exist\"; exit 1;}"
+sudo awk -v replacement="$repl_text" '
+    {
+        count += gsub(/exit 1/, "exit 1")
+        if (count > 11 && sub(/exit 1/, replacement)){
+            count = -999
+        }
+        print
+    }
+    ' "$filename" > temp_file && sudo mv temp_file "$filename"
+chmod +x /sbin/resolvconf
 # default rsyslog version is 8.2110.0 which has a bug on log rate limit,
 # use backport version
 sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -t bullseye-backports -y install rsyslog
